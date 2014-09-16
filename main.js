e853fc937c018d050f3d87cdf2d57bbd60625a7a
ewanck <e.cass-kavanagh@hotmail.co.uk>
2014-09-16 17:15:15 +0100

    removingphoto

--- main.js
+++ main.js
@@ -20,15 +20,23 @@ $(document).ready(function() {
 
     var webcam_sound = false;
 
+    Webcam.attach("#webcam-cont");
+
     webcam_video.on("loadedmetadata", function() {
 	webcam_video.mute();
 	webcam_video.play('75');
+	webcam_video.cue(492, function() {
+	    $("#webcam-cont").css("opacity","1");
+	});
     });
 
     var browser_view = Popcorn("#browser-view");
 //    browser_view.currentTime(20);
     browser_view.on("loadedmetadata", function() {
 	browser_view.play('66.5');
+	browser_view.cue(430, function() {
+	    $("#documents-container").append("<a href='docs/Preliminary_Research.docx'><div class='document'>Preliminary_Research.docx</div></a>");
+	});
     });
 
     $("#mic-button").click(function() {
